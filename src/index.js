8c4bd2ebd165f9dec078c9755ae19e7a16cbe2ec
Kate Shlykova <fonarikkate@gmail.com>
2024-08-21 14:23:53 +0300

    feat: create function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+const { lte } = require("semver");
+
 module.exports = function toReadable (number) {
-  
+  let string = '';
+  let arrOfNumbersLess20 = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+    ];
+    let arrOfTens = [
+        '',
+        '',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety',
+    ];
+    let hundred = Math.floor(number / 100);
+    let numberLess100 = number % 100;
+    let ten = Math.floor(numberLess100 / 10);
+    let ones = numberLess100 % 10;
+
+    if (hundred) {
+        string = arrOfNumbersLess20[hundred] + ' hundred';
+    }
+
+    if (numberLess100 < 20 && numberLess100 > 0) {
+        return string = string ? `${string} ${arrOfNumbersLess20[numberLess100]}` : arrOfNumbersLess20[numberLess100];
+    } else {
+        if (ten) {
+            string = string ? `${string} ${arrOfTens[ten]}` : arrOfTens[ten];
+        }        
+
+        if (ones) {
+            string = string ? `${string} ${arrOfNumbersLess20[ones]}` : arrOfNumbersLess20[ones];
+        } else {
+            string = string ? string : arrOfNumbersLess20[ones];
+        }
+        return string;
+    }
 }
